43567dfc4d192cf7f8b093cf98289cc18fa50d35
Conan Bitter <conan.bitter@gmail.com>
2023-03-02 23:37:46 +0300

    Add flying camera class

---
+++ src/camera.cpp
@@ -0,0 +1,79 @@
+#include "camera.hpp"
+#include <glm/ext.hpp>
+
+const float FlyCamera::pi = glm::pi<float>();
+
+FlyCamera::FlyCamera() {
+    position = glm::vec3(0.0f, 0.0f, 0.0f);
+    forward = glm::vec3(0.0f, 0.0f, -1.0f);
+    right = glm::vec3(1.0f, 0.0f, 0.0f);
+    up = glm::vec3(0.0f, 1.0f, 0.0f);
+    pitch = 0;
+    yaw = 0;
+    ar = 1.0;
+    fov = 45;
+
+    updateProj();
+    updateView();
+}
+
+void FlyCamera::updateProj() {
+    view = glm::lookAt(position, position + forward, up);
+}
+void FlyCamera::updateView() {
+    projection = glm::perspective(glm::radians(fov), ar, 0.1f, 100.0f);
+}
+
+void FlyCamera::updateVectors() {
+    if (pitch >= (pi / 2 - 0.1)) {
+        pitch = pi / 2 - 0.1;
+    }
+
+    if (pitch <= (-pi / 2 + 0.1)) {
+        pitch = -pi / 2 + 0.1;
+    }
+    forward = glm::normalize(glm::vec3(
+        cos(yaw) * cos(pitch),
+        sin(pitch),
+        sin(yaw) * cos(pitch)));
+    right = glm::normalize(glm::cross(up, forward));
+}
+
+void FlyCamera::setPosition(const glm::vec3 &newPosition) {
+    position = newPosition;
+    updateView();
+}
+
+void FlyCamera::setPosition(float x, float y, float z) {
+    position = glm::vec3(x, y, z);
+    updateView();
+}
+
+void FlyCamera::move(float dForw, float dRight, float dUp) {
+    position += forward * dForw + right * dRight + up * dUp;
+    updateView();
+}
+
+void FlyCamera::setRotation(float hor, float vert) {
+    yaw = hor;
+    pitch = vert;
+    updateVectors();
+    updateView();
+}
+
+void FlyCamera::rotate(float dHor, float dVert) {
+    yaw += dHor;
+    pitch += dVert;
+    updateVectors();
+    updateView();
+}
+
+void FlyCamera::setAspectRatio(float newAspectRatio) {
+    ar = newAspectRatio;
+    updateProj();
+}
+
+void FlyCamera::setFOV(float newFov) {
+    fov = newFov;
+    updateProj();
+}
---
+++ src/camera.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class FlyCamera {
+   private:
+    glm::mat4x4 projection;
+    glm::mat4x4 view;
+
+    glm::vec3 position;
+    glm::vec3 forward;
+    glm::vec3 right;
+    glm::vec3 up;
+
+    float pitch;
+    float yaw;
+    float ar;
+    float fov;
+
+    static const float pi;
+
+    void updateProj();
+    void updateView();
+    void updateVectors();
+
+   public:
+    FlyCamera();
+
+    glm::mat4x4 getProjection() { return projection; }
+    glm::mat4x4 getView() { return view; }
+
+    void setPosition(const glm::vec3 &newPosition);
+    void setPosition(float x, float y, float z);
+    void move(float dForw, float dRight, float dUp);
+
+    void setRotation(float hor, float vert);
+    void rotate(float dHor, float dVert);
+
+    void setAspectRatio(float newAspectRatio);
+    void setFOV(float newFov);
+};
